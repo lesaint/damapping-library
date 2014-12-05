e4a33654604adce87e720e026ad6c0941b4ab006
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2014-12-05 11:28:44 +0100

    add empty EnumEnumMapper with javadoc
    as a reminder

---
+++ src/main/java/fr/javatronic/damapping/toolkit/enums/EnumEnumMapper.java
@@ -0,0 +1,9 @@
+package fr.javatronic.damapping.toolkit.enums;
+
+/**
+ * EnumEnumMapper - This class provides the developer with a convenient way to map an Enum to and from another one.
+ *
+ * @author Sébastien Lesaint
+ */
+public class EnumEnumMapper {
+}
