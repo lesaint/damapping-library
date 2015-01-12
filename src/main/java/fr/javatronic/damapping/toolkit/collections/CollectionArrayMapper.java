7e7794a081b65b86a82c4e79d065ed37ab1a62d1
Sébastien Lesaint <sebastien.lesaint@gmail.com>
2015-01-12 22:18:03 +0100

    add support for MappingDefaults to CollectionArrayMapper
    add override for existing method that supports MappingDefaults

--- src/main/java/fr/javatronic/damapping/toolkit/collections/CollectionArrayMapper.java
+++ src/main/java/fr/javatronic/damapping/toolkit/collections/CollectionArrayMapper.java
@@ -15,22 +15,25 @@
  */
 package fr.javatronic.damapping.toolkit.collections;
 
+import fr.javatronic.damapping.toolkit.MappingDefaults;
+
 import java.util.Collection;
 import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import static fr.javatronic.damapping.toolkit.MappingDefaults.defaultTo;
+
 /**
  * CollectionArrayMapper - This class provides the developer with a convenient way to map a Collection from and to
  * an array.
- *
- * TODO: add ToArray returning null for null and/or empty collection
+ * <p/>
  * TODO: java8 add ToArrayOptional
  *
  * @author Sébastien Lesaint
  */
-public class CollectionArrayMapper {
-  private static final Object[] EMPTY_ARRAY = new Object[0];
+public abstract class CollectionArrayMapper {
+  private static final MappingDefaults<?> EMPTY_ARRAY_DEFAULTS = defaultTo(new Object[0]);
 
   /**
    * Transforms a collection into an array, returns an empty array when the specified collection is {@code null} or
@@ -41,7 +44,7 @@ public class CollectionArrayMapper {
    * </p>
    *
    * @param collection a collection of type {@code T} or {@code null}
-   * @param <T> a type
+   * @param <T>        a type
    *
    * @return a array of type {@code T}
    */
@@ -55,38 +58,128 @@ public class CollectionArrayMapper {
   }
 
   /**
-   * Transforms a collection into an array, returns an empty array when the specified collection is empty and the
-   * specified {@code defaultValue} if the collection is empty.
+   * Private method isolating SuppressWarnings annotation to the smallest piece of code possible.
+   */
+  @SuppressWarnings("unchecked")
+  private static <T> T[] emptyArray() {
+    return (T[]) EMPTY_ARRAY_DEFAULTS.whenNull();
+  }
+
+  /**
+   * Transforms a collection into an array, returns the specified defaultValue when the collection is empty or
+   * {@code null}.
    * <p>
    * If the collection makes any guarantees as to what order its elements are returned by its iterator, the elements in
    * the returned array will respect the same order.
    * </p>
    *
-   * @param collection a collection of type {@code T} or {@code null}
-   * @param defaultValue a array of {@code T}
-   * @param <T> a type
+   * @param collection   a collection of type {@code T} or {@code null}
+   * @param defaultValue a array of {@code T} or {@code null}
+   * @param <T>          a type
    *
    * @return a array of type {@code T}
    *
-   * @throws NullPointerException if the {@¢ode defaultValue} is {@code null}
+   * @throws NullPointerException if the {@code defaultValue} is {@code null}
    */
   @Nonnull
-  public static <T> T[] toArray(@Nullable Collection<T> collection, @Nonnull T[] defaultValue) {
+  public static <T> T[] toArray(@Nullable Collection<T> collection, @Nullable T[] defaultValue) {
+    return toArray(collection, defaultTo(defaultValue));
+  }
+
+  /**
+   * Transforms a collection into an array, returns the default returned by the specified defaultValue methods
+   * {@link MappingDefaults#whenNull()} and {@link MappingDefaults#whenEmpty()} when the collection
+   * is {@code null} or empty.
+   * <p>
+   * If the collection makes any guarantees as to what order its elements are returned by its iterator, the elements in
+   * the returned array will respect the same order.
+   * </p>
+   * <p>
+   * For convenience, methods {@link #defaultToEmpty()} and {@link #defaultToNull()} can be used as second
+   * argument of this method to, respectively, returns an empty array when collection is {@code null} or empty
+   * (in which case calling this method is totally equivalent to calling method {@link #toArray(java.util.Collection)}
+   * or return a {@code null} value when collection is {@code null} or empty.
+   * </p>
+   *
+   * @param collection   a collection of type {@code T} or {@code null}
+   * @param defaultValue a {@link MappingDefaults} of type {@code T[]}
+   * @param <T>          a type
+   *
+   * @return a array of type {@code T}
+   *
+   * @throws NullPointerException if the {@code defaultValue} is {@code null}
+   * @see #defaultToEmpty()
+   * @see #defaultToNull()
+   */
+  public static <T> T[] toArray(@Nullable Collection<T> collection,
+                                @Nonnull MappingDefaults<T[]> defaultValue) {
     Objects.requireNonNull(defaultValue);
-    if (collection == null || collection.isEmpty()) {
-      return defaultValue;
+    if (collection == null) {
+      return defaultValue.whenNull();
+    }
+
+    if (collection.isEmpty()) {
+      return defaultValue.whenEmpty();
     }
 
     return toArrayImpl(collection);
   }
 
+  /**
+   * Private method isolating SuppressWarnings annotation to the smallest piece of code possible.
+   */
   @SuppressWarnings("unchecked")
   private static <T> T[] toArrayImpl(Collection<T> collection) {
-    return (T[]) collection.toArray(new Object[collection.size()]);
+    return collection.toArray((T[]) new Object[collection.size()]);
   }
 
+  /**
+   * Creates a MappingDefaults that returns a {@code null} array of type {@code T} in every case.
+   * <p>
+   * This method can also be used to create a MappingDefaults that returns an {@code null} array in only one case. eg.:
+   * <pre>
+   * MappingDefaults<String[]> defaults = CollectionArrayMapper.<String>defaultToNull().withEmptyDefault(new String[]{"ERROR"});
+   * </pre>
+   * </p>
+   *
+   * @param <T> any type
+   *
+   * @return a {@link MappingDefaults}
+   */
+  public static <T> MappingDefaults<T[]> defaultToNull() {
+    return nullArrayDefaultsImpl();
+  }
+
+  /**
+   * Private method hiding SuppressWarnings annotation from public API of the class.
+   */
   @SuppressWarnings("unchecked")
-  private static <T> T[] emptyArray() {
-    return (T[]) EMPTY_ARRAY;
+  private static <T> MappingDefaults<T> nullArrayDefaultsImpl() {
+    return (MappingDefaults<T>) MappingDefaults.defaultToNull();
+  }
+
+  /**
+   * Creates a MappingDefaults that returns an empty array of type {@code T} in every case.
+   * <p>
+   * This method can also be used to create a MappingDefaults that returns an empty array in only one case. eg.:
+   * <pre>
+   * MappingDefaults<String[]> defaults = CollectionArrayMapper.<String>defaultToEmpty().withNullDefault(null);
+   * </pre>
+   * </p>
+   *
+   * @param <T> any type
+   *
+   * @return a {@link MappingDefaults}
+   */
+  public static <T> MappingDefaults<T[]> defaultToEmpty() {
+    return emptyArrayDefaultsImpl();
+  }
+
+  /**
+   * Private method hiding SuppressWarnings annotation from public API of the class.
+   */
+  @SuppressWarnings("unchecked")
+  private static <T> MappingDefaults<T[]> emptyArrayDefaultsImpl() {
+    return (MappingDefaults<T[]>) EMPTY_ARRAY_DEFAULTS;
   }
 }
---
+++ src/test/java/fr/javatronic/damapping/toolkit/collections/CollectionArrayMapper_Collection_Test.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (C) 2014 Sébastien Lesaint (http://www.javatronic.fr/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.javatronic.damapping.toolkit.collections;
+
+import fr.javatronic.damapping.toolkit.MappingDefaults;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.collections.Sets;
+
+import static fr.javatronic.damapping.toolkit.collections.CollectionArrayMapper.defaultToEmpty;
+import static fr.javatronic.damapping.toolkit.collections.CollectionArrayMapper.defaultToNull;
+import static fr.javatronic.damapping.toolkit.collections.CollectionArrayMapper.toArray;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * CollectionArrayMapper_Collection_Test -
+ *
+ * @author Sébastien Lesaint
+ */
+public class CollectionArrayMapper_Collection_Test {
+
+  private static final List<String> STRING_LIST = Arrays.asList("A", "B", "C");
+  private static final List<String> EMPTY_LIST = Collections.emptyList();
+
+  @Test
+  public void toArray_from_collection_returns_empty_array_from_null_collection() throws Exception {
+    assertThat(toArray(null)).isEmpty();
+  }
+
+  @Test(dataProvider = "empty_collections")
+  public <T> void toArray_from_collection_returns_empty_array_from_empty_collection(Collection<T> collection) throws Exception {
+    assertThat(toArray(collection)).isEmpty();
+  }
+
+  @DataProvider
+  public <T> Object[][] empty_collections() {
+    return new Object[][] {
+        {new ArrayList<T>()},
+        {new LinkedList<T>()},
+        {Sets.<T>newHashSet() },
+        {new LinkedHashSet<T>() },
+    };
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test(dataProvider = "non_empty_collections")
+  public <T> void toArray_from_collection_returns_array_from_Collection_toArray_method(Collection<T> collection) throws Exception {
+    assertThat(toArray(collection)).containsExactly((T[]) collection.toArray());
+  }
+
+  @DataProvider
+  public Object[][] non_empty_collections() {
+    return new Object[][] {
+        {STRING_LIST},
+        {new LinkedList<>(STRING_LIST)},
+        {Sets.newHashSet(STRING_LIST) },
+        {new LinkedHashSet<>(STRING_LIST) },
+    };
+  }
+
+  @Test
+  public void toArray_with_default_from_collection_returns_null_when_collection_is_null_or_empty() throws Exception {
+    assertThat(toArray(null, (String[]) null)).isNull();
+    assertThat(toArray(EMPTY_LIST, (String[]) null)).isNull();
+  }
+
+  @Test
+  public void toArray_with_default_returns_default_from_null_collection() throws Exception {
+    String[] defaultValue = new String[] { "default" };
+
+    assertThat(toArray(null, defaultValue)).isSameAs(defaultValue);
+  }
+
+  @Test(dataProvider = "empty_collections")
+  public void toArray_with_default_returns_default_from_empty_collections(Collection<String> emptyCollection) throws Exception {
+    String[] defaultValue = new String[] { "default" };
+
+    assertThat(toArray(emptyCollection, defaultValue)).isSameAs(defaultValue);
+  }
+
+  /*=================*
+   * MappingDefaults *
+   *=================*/
+  @Test(expectedExceptions = NullPointerException.class)
+  public void toArray_with_MappingDefaults_throws_NPE_if_MappingDefaults_is_null() throws Exception {
+    toArray(STRING_LIST, (MappingDefaults<String[]>) null);
+  }
+
+  @Test
+  public void toArray_with_MappingDefaults_discriminate_null_or_empty_collection() throws Exception {
+    String[] nullDefault = {"NULL_COLLECTION"};
+    String[] emptyDefault = {"EMPTY_COLLECTION"};
+    MappingDefaults<String[]> mappingDefaults = MappingDefaults.defaultTo(nullDefault);
+
+    assertThat(toArray(null, mappingDefaults.withEmptyDefault(emptyDefault))).isEqualTo(nullDefault);
+    assertThat(toArray(EMPTY_LIST, mappingDefaults.withEmptyDefault(emptyDefault))).isEqualTo(emptyDefault);
+  }
+
+  @Test
+  public void defaultToNull_returns_MappingDefaults_with_null_defaults() throws Exception {
+    assertThat(defaultToNull().whenNull()).isNull();
+    assertThat(defaultToNull().whenEmpty()).isNull();
+  }
+
+  @Test
+  public void defaultToEmpty_returns_MappingDefaults_with_empty_array_defaults() throws Exception {
+    assertThat(defaultToEmpty().whenNull()).isEmpty();
+    assertThat(defaultToEmpty().whenEmpty()).isEmpty();
+  }
+}
